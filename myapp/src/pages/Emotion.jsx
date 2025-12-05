ea38f46d21b9e435a811ba1f850cb55a90868654
Atharv542 <atharvragdwal05@gmail.com>
2025-12-05 23:38:33 +0530

    Added changes

--- myapp/src/pages/Emotion.jsx
+++ myapp/src/pages/Emotion.jsx
@@ -32,7 +32,7 @@ export default function Emotion() {
   const sendToBackend = async (base64Image) => {
     setLoading(true);
     try {
-      const res = await fetch(" https://moodtune-1-w0jn.onrender.com/predict", {
+      const res = await fetch("https://moodtune-1-w0jn.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ img: base64Image }),
