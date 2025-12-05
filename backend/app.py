b6f647561fcbff4f32b5623b8e76e50f7d93bb5c
Atharv542 <atharvragdwal05@gmail.com>
2025-12-06 00:32:23 +0530

    Added changes

--- backend/app.py
+++ backend/app.py
@@ -30,7 +30,7 @@ SPOTIFY_CLIENT_SECRET = "a66a684a4b5a4cfc8d73f97636f49d9e"
 # App init
 # ---------------------------
 app = Flask(__name__)
-CORS(app, origins=["https://mood-tune-nine.vercel.app/"])
+CORS(app, resources={r"/*": {"origins": "https://mood-tune-nine.vercel.app"}})
 
 
 # ---------------------------
